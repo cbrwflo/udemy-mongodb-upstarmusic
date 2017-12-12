cd0f4fafc7be4269a80067bc97cd45a052ef4915
Brown <Carl.Brown194@T-Mobile.com>
2017-12-11 20:36:20 -0800

    Implement search artist function

--- database/queries/SearchArtists.js
+++ database/queries/SearchArtists.js
@@ -9,4 +9,45 @@ const Artist = require('../models/artist');
  * @return {promise} A promise that resolves with the artists, count, offset, and limit
  */
 module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
+  const artistQuery = Artist
+    .find(buildQuery(criteria))
+    .sort({ [sortProperty]: 1 }) //ES6 interprolated keys
+    .skip(offset)
+    .limit(limit);
+    
+    return Promise.all([artistQuery, Artist.count()])
+      .then((results) => {
+        return {
+          all: results[0],
+          count: results[1],
+          offset,
+          limit
+        };
+      });
 };
+
+const buildQuery = (criteria) => {
+  const query = {};
+  
+  if (criteria.name !== '') {
+    query.$text = {
+      $search: criteria.name
+    };
+  }
+
+  if (criteria.age) {
+    query.age = { 
+      $gte: criteria.age.min, 
+      $lte: criteria.age.max
+    };
+  }
+
+  if (criteria.yearsActive) {
+    query.yearsActive = { 
+      $gte: criteria.yearsActive.min, 
+      $lte: criteria.yearsActive.max
+    };
+  }
+
+  return query;
+}
